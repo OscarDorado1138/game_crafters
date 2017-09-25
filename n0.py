9678013ebdaf97c8a344d5efe862803f85abadf9
OscarDorado1138 <OscarDorado1138@Berkeley.edu>
2017-09-25 13:16:41 -0700

    Added Remoteness

--- n0.py
+++ n0.py
@@ -3,7 +3,9 @@ def main():
     Initial_Position = 4
     Player = 0
     outcome = solve(Initial_Position, Primitive, Generate_Moves, Do_Move, Player)
-    print("Initial Position of " + str(Initial_Position) +" results in a " + str(outcome))
+    remoteness = outcome[1]
+
+    print("Initial Position of " + str(Initial_Position) +" results in a " + str(outcome[0]) + " in " + str(remoteness) +" moves.")
 
 
 
@@ -11,32 +13,53 @@ def solve(initial_position, primitive, generate_moves, do_move, current_player):
 
 		global_moves = generate_moves(initial_position)
 
-		result = "LOSS"
+		remoteness = 0
+		loss_t = 0
+		win_t = 100
+
+		result = "NULL"
 
 		for move in global_moves:
-			if (branch(1 - current_player, Do_Move(initial_position, move)) == "LOSS"):
-				result = "WIN"
-		return result
+			min_result = branch(1 - current_player, Do_Move(initial_position, move), remoteness)
+			if (min_result[0] == "WIN" and win_t > min_result[1]):
+				win_t = min_result[1]
+			if (min_result[0] == "LOSS"):
+				result = "LOSS"
+				if (min_result[1] > loss_t):
+					loss_t = min_result[1] 
 
-def branch(current_player, position):
-	
+		if(result == "WIN"):
+			remoteness = win_t
+		else:
+			remoteness = loss_t
+
+		return (result, remoteness)
+
+def branch(current_player, position, remoteness):
+	remoteness += 1
 	if (Primitive(position, current_player) == "Continue"):
-
 		moves = Generate_Moves(position)
 		for move in moves:
-			return branch(1 - current_player, Do_Move(position, move))
+			return branch(1 - current_player, Do_Move(position, move), remoteness)
 	else:
-		return Primitive(position, current_player)
+		return (Primitive(position, current_player), remoteness)
 
 
 def Primitive(position, current_player):
 	if (position == 0):
 		if (current_player == 1):
-			return "LOSS"
-		else:
+			print("no")
 			return "WIN"
+
+
+		else:
+			#print("yes")
+
+			return "LOSS"
+
 	else:
 		return "Continue"
+
 
 
 def Generate_Moves(position):
